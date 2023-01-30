f4959decdf66b941cadd8e0c667bdeb38fd35a2e
cassiano yasumitsu <cassiano.yasumitsu@wealth-park.com>
2023-01-30 11:33:45 +0900

    feat: structure

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from hashlib import md5
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db, app
+from app import db
 from flask_login import UserMixin
 from time import time
 import jwt
